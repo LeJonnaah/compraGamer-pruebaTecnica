fdc4fe07a7c3dd63f4775efec392a9fef8c67201
Jandueza <jonathanandueza1201@gmail.com>
2023-06-02 17:55:40 -0300

    cart styles, delete items & total price

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -11,7 +11,22 @@ export class CartComponent implements OnInit {
 
   constructor(private cartService: CartService) { }
 
+  // Método para eliminar un producto del carrito
+  removeFromCart(product: any) {
+    this.cartService.removeFromCart(product);
+  }
+
+  // Método para obtener el precio total del carrito
+  getTotalPrice() {
+    let total = 0;
+    this.cartItems.forEach(item => {
+      total += item.precio;
+    });
+    return total;
+  }
+
   ngOnInit(): void {
+    // Al inicializar el componente, obtenemos los elementos del carrito desde el servicio
     this.cartItems = this.cartService.getCartItems();
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -4,32 +4,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
-  private cartItems: any[] = [];
-  totalItems = 0;
+  private cartItems: any[] = []; // Array para almacenar los elementos del carrito
+  totalItems = 0; // Contador de elementos del carrito
+
   constructor() { }
 
+  // Método para agregar un producto al carrito
   addToCart(product: any): void {
-    this.cartItems.push(product);
-    this.totalItems++;
+    this.cartItems.push(product); // Agregar el producto al array de elementos del carrito
+    this.totalItems++; // Incrementar el contador de elementos del carrito
   }
 
+  // Método para eliminar un producto del carrito
   removeFromCart(product: any): void {
-    const index = this.cartItems.indexOf(product);
-    if (index !== -1) {
-      this.cartItems.splice(index, 1);
-    }
+    this.cartItems.splice(product, 1); // Eliminar el producto del array de elementos del carrito
+    this.totalItems--; // Decrementar el contador de elementos del carrito
   }
 
+  // Método para obtener todos los elementos del carrito
   getCartItems(): any[] {
     return this.cartItems;
   }
 
+  // Método para limpiar el carrito (eliminar todos los elementos)
   clearCart(): void {
     this.cartItems = [];
   }
 
+  // Método para obtener la cantidad de elementos en el carrito
   getCartItemCount(): number {
     return this.cartItems.length;
   }
-  
 }
